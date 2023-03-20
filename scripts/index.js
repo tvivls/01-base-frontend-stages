c22b774e9c57084d799441af64dddb8d27440464
Valentina <valya.cherednichenko.03@mail.ru>
2023-03-20 20:29:53 +0300

    scripts for spa

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,10 @@
-const yandexMap = () => {
-    const map = new ymaps.Map('yandexmap', {
-        center: [56.737155, 37.165405],
-        zoom: 16,
-    });
-    const marker = new ymaps.Placemark([56.737155, 37.165405], {
-        hintContent: 'Replace',
-        balloonContent: 'This is my home',
-    });
-    map.geoObjects.add(marker);
-}
-
-window.onload = function () {
-    const loader = document.querySelector('#preloader');
-    loader.classList.add('visually-hidden');
-    // loader.style.display = "none";
-    const yaMap = document.querySelector('#yandexmap');
-    yaMap.style.display = 'block';
-    ymaps.ready(yandexMap);
-};
+(function () {
+    function init() {
+        let router = new Router([
+            new Route('main', 'main.html', true),
+            new Route('map', 'map.html'),
+            new Route('timer', 'timer.html')
+        ]);
+    }
+    init();
+}());
