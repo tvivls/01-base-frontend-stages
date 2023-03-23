a73164f3bb513d4437f9d423b84c7b5a630e4c13
Valentina <valya.cherednichenko.03@mail.ru>
2023-03-23 23:17:00 +0300

    fix: отображение map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,12 +1,19 @@
-let marker;
-export default function init() {
-    let map = new ymaps.Map('yandexmap', {
+function initMap() {
+    const map = new ymaps.Map('yandexmap', {
         center: [56.73630928543602,37.16224752339015],
         zoom: 16,
     });
-    marker = new ymaps.Placemark([56.735205, 37.164777], {
+    const marker = new ymaps.Placemark([56.735205, 37.164777], {
         hintContent: 'Расположение',
         balloonContent: 'Дом'
     });
     map.geoObjects.add(marker);
+}
+export default function displayMap() {
+    ymaps.ready(initMap).then(() => {
+        const mapDiv = document.getElementById('yandexmap');
+        const preloader = document.getElementById('preloader');
+        preloader.classList.add('visually-hidden');
+        mapDiv.style.display = 'block';
+    })
 }
