b2144f0fb6d90a3743691882be50880808d765ac
Valentina <valya.cherednichenko.03@mail.ru>
2023-03-24 00:13:06 +0300

    fix: timer

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,19 +1,30 @@
-let startTime = null;
-let currentTime = null;
+let startTime;
+let currentTime;
 let elapsedTime = 0;
+let timerId;
 
-function updateTimer() {
+export default function displayTimer() {
     currentTime = new Date();
     elapsedTime = Math.floor((currentTime - startTime) / 1000);
-    let hours = Math.floor(elapsedTime / 3600).toString().padStart(2, '0');
-    let minutes = Math.floor((elapsedTime % 3600) / 60).toString().padStart(2, '0');
-    let seconds = (elapsedTime % 60).toString().padStart(2, '0');
-    localStorage.setItem('timer', `${hours}:${minutes}:${seconds}`);
-
-    setTimeout(updateTimer, 1000);
+    const hours = Math.floor(elapsedTime / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((elapsedTime % 3600) / 60).toString().padStart(2, '0');
+    const seconds = (elapsedTime % 60).toString().padStart(2, '0');
+    if (window.location.hash === '#timer') {
+        const timer = document.querySelector('#time');
+        timer.textContent = `${hours}:${minutes}:${seconds}`;
+    }
+}
+function startTimer() {
+    timerId = setInterval(displayTimer, 1000);
 }
 
-window.addEventListener('load', function() {
+function stopTimer() {
+    clearInterval(timerId);
+}
+
+window.addEventListener('load', () => {
     startTime = new Date();
-    setInterval(updateTimer, 1000);
+    startTimer();
 });
+
+window.addEventListener('beforeunload', stopTimer);
