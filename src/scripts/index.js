3b145b3b73a82ef5e634b01adba87103f5511f93
Valentina <valya.cherednichenko.03@mail.ru>
2023-03-25 02:17:32 +0300

    fix: исправлен let

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import Router from './router.js';
 import Route from './route.js';
 function init() {
-    new Router([
+    const router = new Router([
         new Route('main', 'main.html', true),
         new Route('map', 'map.html'),
         new Route('timer', 'timer.html')
