9a4176abeca79600e67f123bc188a88d3f311895
Valentina <valya.cherednichenko.03@mail.ru>
2023-05-07 18:01:23 +0300

    fix: activePage

--- src/scripts/activePage.js
+++ src/scripts/activePage.js
@@ -1,8 +1,5 @@
-const links = document.querySelectorAll('.nav-item');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        const activeElement = document.querySelector('.active-page');
-        activeElement.classList.remove('active-page');
-        link.classList.add('active-page');
-    });
-});
+export default function activePage(link) {
+    const activeElement = document.querySelector('.active-page');
+    activeElement.classList.remove('active-page');
+    link.classList.add('active-page');
+};
