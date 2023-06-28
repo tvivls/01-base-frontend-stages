85920d03dad52e6d92a2771da7ce58d355350265
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-06-28 15:29:40 +0300

    fix: исправлено дублирование

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,18 +1,22 @@
 const startTime = new Date();
 let timerId;
 
+function convertTime(time) {
+    return time.toString().padStart(2, '0');
+}
+
 export default function displayTimer() {
     const elapsedTime = Math.floor((new Date() - startTime) / 1000);
-    const hours = Math.floor(elapsedTime / 3600).toString().padStart(2, '0');
-    const minutes = Math.floor((elapsedTime % 3600) / 60).toString().padStart(2, '0');
-    const seconds = (elapsedTime % 60).toString().padStart(2, '0');
+    const hours = Math.floor(elapsedTime / 3600);
+    const minutes = Math.floor((elapsedTime % 3600) / 60);
+    const seconds = (elapsedTime % 60);
 
     const path = window.location.pathname;
     const isTimer = path.slice(path.lastIndexOf('/') + 1) === 'timer';
     if (isTimer) {
         startTimer();
         const timer = document.querySelector('#time');
-        timer.textContent = `${hours}:${minutes}:${seconds}`;
+        timer.textContent = `${convertTime(hours)}:${convertTime(minutes)}:${convertTime(seconds)}`;
     }
     if (!isTimer && timerId !== 0) {
         stopTimer();
